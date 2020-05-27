5f8e9f4128a4ed6dab3017a581aa754d68663e80
Penn M Diba <pdiba@oxbowschool.org>
2020-05-27 16:34:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |string|
-    puts "Hello {#string}!"
+    puts "Hello #{string}!"
   end
 end
 
@@ -11,4 +11,5 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do 
 end
